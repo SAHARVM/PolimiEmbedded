e4d08defe0d2fced9ef6b94db7e60fcb0ca4c020
Arturo Montufar <arturo.montufar@mail.polimi.it>
2017-01-30 18:56:33 +0100

    Working Version 1 - LED blink
    Project successfully compiled and running on VESC Board

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # PolimiEmbedded
-
-Embedded System and Advanced Operating Systems course at Politecnico di Milano - 
-Miosix OS porting to VESC Board version 4.12
+Embedded System course at Polimi
--- miosix-kernel/main.cpp
+++ miosix-kernel/main.cpp
@@ -5,7 +5,19 @@
 using namespace std;
 using namespace miosix;
 
+typedef Gpio<GPIOC_BASE,4>  greenLED;
+
 int main()
 {
-    //iprintf("Hello world, write your application here\n");
+    greenLED::mode(Mode::OUTPUT);
+        
+    while(1) 
+    {
+        greenLED::high();
+        sleep(1);
+        greenLED::low();
+        sleep(1);
+    }
+  
+  return 0;
 }
